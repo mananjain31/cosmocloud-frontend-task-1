e8346f982f179dcac030176266083fc67195c185
mananjain31 <mananjain31jan@gmail.com>
2024-10-15 02:17:44 +0530

    :bug: Interface writing textarea now highlighting red on error

--- src/components/WriteModal.tsx
+++ src/components/WriteModal.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import Paper from "./Paper";
 import { Button, IconButton } from "./Buttons";
 import { Property } from "./PropertyInput";
@@ -14,15 +14,19 @@ const WriteModal: FC<WriteModalProps> = ({ open, onClose, setProperty }) => {
   const [value, setValue] = useState("");
   const [error, setError] = useState(false);
   const validateAndImport = () => {
-    const property: Property = interfaceStringToProperty(value);
-    setProperty(property);
-    onClose();
     try {
+      const property: Property = interfaceStringToProperty(value);
+      setProperty(property);
+      onClose();
     } catch (e) {
       console.error(e);
       setError(true);
     }
   };
+
+  useEffect(() => {
+    setError(false);
+  }, [value]);
 
   return open ? (
     <div className="absolute inset-0 bg-slate-100 flex justify-center items-start py-10">
@@ -35,7 +39,7 @@ const WriteModal: FC<WriteModalProps> = ({ open, onClose, setProperty }) => {
         </header>
         <main>
           <textarea
-            style={error ? { border: "1px solid red" } : {}}
+            style={error ? { border: "1px solid #ff999c" } : {}}
             className="w-full h-96 bg-gray-50 p-4 rounded-md border focus:shadow-md outline-none"
             placeholder="Write your interface here"
             value={value}
